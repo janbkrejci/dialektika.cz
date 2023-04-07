fc74d077858ac5cf2d208c324b9143f4ef6eee0d
Jan B. Krejčí <janbkrejci@gmail.com>
2023-04-07 14:08:01 +0000

    update

---
+++ rebuild.sh
@@ -0,0 +1,18 @@
+rm -rf archive
+rm -rf blog
+rm -rf css
+rm -rf dialektika
+rm -rf gdpr
+rm -rf img
+rm -rf js
+rm -rf login
+rm -rf program
+rm -rf stanovy
+rm -rf stanoviska
+rm -rf tags
+rm 404.html
+rm index.html
+rm sitemap.xml
+cd ../frontend
+hugo
+cd ../pb_public
